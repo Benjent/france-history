4dd68a789487199de35208e9ce6d1d11fe690fe2
Benjent <Benjent@hotmail.fr>
2022-04-25 13:00:20 +0200

    Add annexations

--- src/db/annexations.js
+++ src/db/annexations.js
@@ -9,6 +9,31 @@ const annexations = [
         start: '0536-01-01',
         end: '0536-01-01',
     },
+    {
+        content: "Partage du royaume des Francs en 4 : royaume d'Orléans, royaume de Paris, royaume de Reims, royaume de Soissons",
+        start: '0536-01-01',
+        end: '0536-01-01',
+    },
+    {
+        content: "Réunification du royaume des Francs",
+        start: '0561-01-01',
+        end: '0561-01-01',
+    },
+    {
+        content: "Partage du royaume des Francs en 4 : royaume d'Orléans, royaume de Paris, royaume de Reims, royaume de Soissons",
+        start: '0561-12-31',
+        end: '0561-12-31',
+    },
+    {
+        content: "Réunification de l'Austrasie et la Bourgogne",
+        start: '0612-12-31',
+        end: '0612-12-31',
+    },
+    {
+        content: "Réunification de la Neustrie et l'Austrasie",
+        start: '0673-12-31',
+        end: '0673-12-31',
+    },
 ]
 
 export default annexations
